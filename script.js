7fd3dbcdde24926ae36ede4de72bcdd9b09b6e41
Janaina Silva <89433835+ajanainasilva@users.noreply.github.com>
2021-09-19 22:37:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamburguer =document.querySelector(".hamburguer");
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-menu")
+);
+const contactContent ="Para novas oportunidades de trabalho, novidades, tomar um café, me contate através do telefone acima, ou me envie um email";
